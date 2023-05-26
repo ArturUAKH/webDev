2f72f6e1e9061df5b9ce657910326a8d30ab2aa6
Artur <arti404049@gmail.com>
2023-05-26 11:21:35 +0300

    ArrSomePractice

---
+++ JavaScript/sortArrays/index.js
@@ -0,0 +1,107 @@
+"use strict";
+
+//FILTER
+//Создаем условие и возвращаем элемент .Вернутся только те элементы которы прошли условие
+
+const names = ["Ann", "Jonh", "Segey", "Oleg", "Valdemar"];
+
+const shortNames = names.filter((name) => {
+    return name.length < 5;
+});
+console.log(shortNames);
+
+//MAP
+//Применяет коллбэк к каждому элементу и возвращает новый массив
+//В єтом примере сразу записываем результат в изначальный массив
+//Но лучше создать отдельную переменную и поместить результаты туда
+let answers = ["aNN", "DmitrIy", "ivaN"];
+
+answers = answers.map((answer) => answer.toLowerCase());
+
+console.log(answers);
+
+//EVERY / SOME
+
+const findNum = ["Vasya", 5, "Sonya"];
+
+console.log(findNum.some((item) => typeof item === "number"));
+
+const numbers = [5, 44, 83, "str"];
+
+console.log(numbers.every((num) => typeof num === "number"));
+
+//REDUCE
+//Метод собирает массив в одно целое и возвращает результат
+
+const fruits = ["apple", "pear", "plum"];
+
+const resultFruits = fruits.reduce((sum, current) => {
+    return `${sum}, ${current}`;
+});
+console.log(resultFruits);
+
+const numbersReduce = [5, 65, 80, 25];
+
+const resultReduce = numbersReduce.reduce((sum, current) => {
+    return sum + current;
+}, 10); //В этом приимере мы также передали начальное значение в коллбэк - 10 .
+//начальное значение будет подставлено вместо sum при первом вызове
+console.log(resultReduce);
+
+//Используем chaining для обработки обьекта сразу несколькими методами
+const testObj = {
+    Ann: "person",
+    Vova: "person",
+    Cat: "animal",
+    Dog: "animal"
+};
+
+const newArr = Object.entries(testObj)
+    .filter((item) => item[1] === "person")
+    .map((item) => item[0]);
+console.log(newArr);
+
+const films = [
+    {
+        name: "Titanic",
+        rating: 9
+    },
+    {
+        name: "Die hard 5",
+        rating: 5
+    },
+    {
+        name: "Matrix",
+        rating: 8
+    },
+    {
+        name: "Some bad film",
+        rating: 4
+    }
+];
+// function showGoodFilms(arr){
+//     return arr.filter((film) => film.rating >= 8);
+// }
+
+// console.log(showGoodFilms(films));
+
+// function showListFilm(arr) {
+//     return arr
+//         .map((item) => item.name)
+//         .reduce((sum, current) => `${sum},${current}`);
+// }
+
+// console.log(showListFilm(films));
+
+function addFilmsId(arr) {
+    return arr.map((item, i) => {
+        item.id = i;
+        return item;
+    });
+}
+let transformedArray = addFilmsId(films);
+
+const checkFilms = (arr) =>
+    arr.every((film) => (film.id || film.id === 0 ? true : false));
+
+console.log(checkFilms(transformedArray));
